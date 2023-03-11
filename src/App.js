75b71d325415422605f79244601addf5985afbd0
Manjupriya-T <manjupriya3110@gmail.com>
2023-03-11 16:26:22 +0530

    teachercrud

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-
+import AddDetails from './Component/AddDetails';
+import DataBoard from './Component/DataBoard';
+import NavBar from './Component/NavBar';
+import EditDetails from './Component/EditDetails';
+import { useState } from 'react';
+import {BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 function App() {
+  const [userdata,setuserData]=useState([])
+  fetch("https://632161eafd698dfa29f6a0c4.mockapi.io/student")
+  .then((data)=>data.json())
+  .then((get)=>console.log(get))
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+          <NavBar/>
+          <Routes>
+      <Route path='/databoard'element={<DataBoard data={{userdata,setuserData}}/>}/>
+    
+      <Route path='/add-details'element={ <AddDetails data={{userdata , setuserData}}/>}/>
+      <Route path='*'element={<Navigate to='/databoard'/>}/>
+      <Route path='/edit-user/:id' element={<EditDetails data={{userdata , setuserData}}/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
